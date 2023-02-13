f51f562512255b0c00f0c5a98fb75329b7816a94
Marcos Gonzalez <gonzalezmarcos_90@hotmail.com>
2023-02-12 23:42:00 -0300

    Subiendo segunda entrega CoderHouse.

--- js/condicion.js
+++ js/condicion.js
@@ -1,89 +1,123 @@
-let users = "marcos";
+// let diaAtencion = "martes";
 
-let diaAtencion = "martes";
+// let horaApertura = 10;
 
-let horaApertura = 10;
+// let horaCierre = 18;
 
-let horaCierre = 18;
+//Validamos si el Usuario ingresado está registrado o no
+// function usersRegister(user) {
 
-function usersRegister(user) {
+//     if (user == users) {
 
-    if (user == users) {
+//         user = user.toLowerCase();
 
-        user = user.toLowerCase();
+//         return user
 
-        return user
+//     } else {
 
-    } else {
+//         return alert("Debe estar registrado. Gracias.");
+//     }
+// }
 
-        return alert("Debe estar registrado. Gracias.");
-    }
-}
+//Validamos si el dia elegido está disponible.
+// function diaTurno(dia) {
 
-function diaTurno(dia) {
+//     dia = dia.toLowerCase();
 
-    dia = dia.toLowerCase();
+//     if (dia == diaAtencion) {
 
-    if (dia == diaAtencion) {
+//         return dia
 
-        return dia
+//     } else {
 
-    } else {
+//         return alert("Día seleccionado no disponible");
+//     }
+// }
 
-        return alert("Día seleccionado no disponible");
-    }
-}
+//Chequeamos que la hora ingresada sea dentro del horario donde se encuentra abierto.
+// function horaTurno(hora) {
 
-function horaTurno(hora) {
+//     hora = parseInt(hora);
 
-    hora = parseInt(hora);
+//     if (hora >= horaApertura && hora <= horaCierre) {
 
-    if (hora >= horaApertura && hora <= horaCierre) {
+//         return hora
 
-        return hora
+//     } else {
 
-    } else {
+//         return alert("Horario no disponible")
+//     }
+// }
 
-        return alert("Horario no disponible")
-    }
-}
+//Inicializamos la pregunta si desea sacar o no un turno para entrar al While
+// let pregunta = " ";
 
-let pregunta = " ";
+// pregunta = pregunta.toLowerCase();
 
-pregunta = pregunta.toLowerCase();
+// pregunta = prompt("Desea set_tun turno? Si o No. ");
 
-while (pregunta != "no") {
+// while (pregunta != "no") {
 
-    pregunta = prompt("Desea solicitar un turno? Si o No. ");
+//     //Atra vez de las variables recopilamos la info recibida por el usuario.
+//     let user = prompt("Ingrese usuario");
 
-    if (pregunta != "no") {
+//     let dia = prompt("Ingrese día. (Únicamente martes)");
 
-        let user = prompt("Ingrese usuario");
+//     let hora = prompt("Ingrese un horario entre las 10 y 18 hs.")
 
-        let dia = prompt("Ingrese día. (Únicamente martes)");
+//     //Validamos que si todas las restricciones son verdaderas, se ejecuten los siguientes console log
+//     if (usersRegister(user) && diaTurno(dia) && horaTurno(hora)) {
 
-        let hora = prompt("Ingrese un horario entre las 10 y 18 hs.")
-
-        if (usersRegister(user) && diaTurno(dia) && horaTurno(hora)) {
-
-            console.log("Su turno ha sido registrado.");
-            console.log("");
-            console.log("----Datos de su turno----");
-            console.log("Nombre usuario: ", user);
-            console.log("Día de turno: ", dia);
-            console.log("Hora: ", hora);
-            console.log("Gracias por su visita.");
-            console.log("");
-
+//         console.log("Su turno ha sido registrado.");
+//         console.log("");
+//         console.log("----Datos de su turno----");
+//         console.log("Nombre usuario: ", users);
+//         console.log("Día de turno: ", dia);
+//         console.log("Hora: ", hora);
+//         console.log("Gracias por su visita.");
+//         console.log("");
 
 
-            pregunta = prompt('Su turno ha sido registrado. Presione "Sí" si desea otro turno y "No" pasa salir');
 
-            continue;
+//         pregunta = prompt('Su turno ha sido registrado. Presione "Sí" si desea otro turno y "No" pasa salir');
 
-        }
-    }
-    else {
-        alert("Muchas gracias por su visita =).")
-    }
-}
+//         continue;
+
+//     }
+
+//     else {
+//         //En caso que no se pueda guardar el turno, saldrá el alert.
+//         alert("Muchas gracias por su visita =).")
+//     }
+//     pregunta = prompt("Desea set_tun turno? Si o No. ");
+
+// }
+
+// /*class Client {
+//   constructor(firstName, lastName, shift) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.shift = shift;
+//   }
+  
+//   getName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+  
+//   getShift() {
+//     return `${this.shift.day} at ${this.shift.time}`;
+//   }
+// }
+
+// class Shift {
+//   constructor(day, time) {
+//     this.day = day;
+//     this.time = time;
+//   }
+// }
+
+// const myShift = new Shift("Monday", "9:00 AM");
+// const myClient = new Client("John", "Doe", myShift);
+
+// console.log(myClient.getName()); // Output: John Doe
+// console.log(myClient.getShift()); // Output: Monday at 9:00 AM
--- js/condicion1.js
+++ js/condicion1.js
@@ -1,55 +1,117 @@
-let users = "marcos";
 
-let diaAtencion = "martes";
+class Cliente {
+    constructor(nombre, apellido, telefono, dia, hora) {
 
-let horaApertura = 10;
-
-let horaCierre = 16;
-
-function reservaTurno(user, dia, hora) {
-
-    hora = parseInt(hora);
-
-    if (user == users) {
-
-        return user
+            this.nombre = nombre,
+            this.apellido = apellido,
+            this.telefono = telefono,
+            this.dia = null,
+            this.hora = null          
     }
-    else if (dia == diaAtencion) {
-
-        return dia
+    get_datos(){
+            //Método para enviar todos los datos del turno solicitado
+            console.log("----Datos de su turno----");
+            console.log("Nombre usuario: ", this.nombre);
+            console.log("Día de turno: ", this.dia);
+            console.log("Hora: ", this.hora);
+            console.log("Gracias por su visita.");
+            console.log("");
     }
-    else if ((hora >= horaApertura) && (hora <= horaCierre)) {
+    set_turno(dia, hora) {
+        //Método para insertar turno en usuario registrado
+            this.dia = dia;
+            this.hora = hora;
+        
+  }
+}
 
-        return hora
-    }
+// Inicializamos la lista de usuarios y creamos uno a modo testing
+const users = [];
 
+users.push(new Cliente("marcos", "gonzalez", 1160361071,))
+
+//Función para buscar si el cliente ya esta registrado
+function buscar_cliente(nombre) {
+    return cliente => cliente.nombre == nombre;
 }
 
 
-let pregunta = prompt('¿Desea algún turno? Si no desea escriba "no"  ').toLowerCase();
+//Inicializamos las variables para entrar al while
 
-while (pregunta != 'no') {
+let nombre = prompt("Ingrese nombre: ");
+nombre = nombre.toLowerCase();
 
-    
-    if (pregunta != 'no') {
-        
-        let usuario = prompt('Ingrese su usuario: ')
-        
-        let diaTurno = prompt('Ingrese su usuario: ')
-        
-        let horaTurno = prompt('Ingrese su usuario: ')
+let pregunta =prompt('Desea sacar un turno?'); 
 
-        let turnoRegistrado = reservaTurno(usuario, diaTurno, horaTurno)
+//Bucle para validar los campor requeridos
+while (pregunta != "no"){ 
+
+    // pregunta = prompt('Si, para solicitar turno. No, para salir.: ')
+    // pregunta = pregunta.toLowerCase();
+
+    //Variable donde almacenamos el usuario ingresado y filtramos si el mismo está registrado o no.
+    let resultado_busqueda = users.find(buscar_cliente(nombre));
+
+    //En caso de no estar registrado, se solicitará registrarse de modo automático.
+    if(resultado_busqueda != undefined){
+
+        let dia = prompt("Ingrese un dia para su turno: ");
+        dia = dia.toLowerCase();
+
+        let diasValido = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes'];
+
+        //Validamos que el dia ingresado sea dentro de los disponibles
+        if (diasValido.includes(dia)) {
+            let hora = parseInt(prompt("Ingrese un horario:"));
         
-        console.log((turnoRegistrado));
-        console.log("----Datos de su turno----");
-        console.log("Bienvenido: ", usuario);
-        console.log("Su turno fue registrado para el día: ", diaTurno);
-        console.log("En el horario de las: ", horaTurno);
-    
-        // alert('Usuario incorrecto o debe registrarse para continuar.')
-        
+            //Verificamos que la hora sea dentro del horario de apertura y cierre    
+            if(hora >= 10 && hora <= 16){
+                
+                //Si todo esta Ok, se imprime las siguientes líneas, junto con los métodos declarados en la class.
+                console.log("Su turno ha sido registrado.");
+                console.log("");
+                resultado_busqueda.set_turno(dia, hora);
+                resultado_busqueda.get_datos();
+                console.log(users);
+                alert("Su turno ha sido registrado.\n En breve le llegará un mensaje al número ingresado. ")
+                break;
+            }
+            else{
+                alert('Horario no disponible. Sólo de 10 a 16hs.')
+            }
+        }
+        else{
+            alert("El día seleccionado no es válido. Por favor ingrese un día de lunes a viernes.");
+        }
     }
-    pregunta = prompt('¿Desea otro turno? Si no desea, escriba "no"  ')
+    else{
 
+        alert("Debe registrarse para solicitar un turno");
+
+        nombre = prompt("Ingrese su nombre: ");
+        nombre = nombre.toLowerCase();
+        if( resultado_busqueda != undefined ){
+            alert("Usuario ya registrado!")
+
+        }
+        else{
+
+            let apellido = prompt("Ingrese apellido: ");
+            apellido = apellido.toLowerCase();
+
+            let telefono = parseInt(prompt("Ingrese telefono: "));
+
+            let nuevo_user = new Cliente(nombre ,apellido , telefono);
+
+            users.push(nuevo_user);
+            console.log(nuevo_user);
+    
+            alert(`Gracias por registrarse ${nombre},\n
+            se ha iniciado sesión.\n
+            A continuacion ingrese "si" para solicitar turno,\n
+            "no" en caso de finalizar.`)
+        pregunta = prompt('"si" para continuar. "no" para salir')
+        }
+    }
+    
 }
